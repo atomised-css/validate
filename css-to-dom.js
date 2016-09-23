aa7a645d5fc727948019f4d78e426735d8653dec
sndrs <a@snd.rs>
2016-09-23 11:52:47 +0100

    first big batch of tests

--- css-to-dom.js
+++ css-to-dom.js
@@ -6,78 +6,155 @@ import selectorParser from 'postcss-selector-parser';
 import uniq from 'lodash.uniq';
 import emmet from 'emmet';
 
-export default async (originalCSS) => {
-    // converts a selector into something that emmet can use
-    function normaliseForEmmet(selector) {
-        selector.walkPseudos(pseudo => {
-            switch (pseudo.value) {
-                case ':first-child': {
-                    const parent = pseudo.parent;
-                    parent.removeChild(pseudo);
-                    parent.last.value += '*2';
-                    break;
-                }
-            }
-        });
+const emmetProfile = emmet.profile.create('test', {
+    place_cursor: false,
+    self_closing_tag: false,
+    compact_bool: false,
+    tag_nl: false,
+});
 
-        selector.walkUniversals(universal => {
-            console.log('hi universal');
+const testString = '£337_H4X0R';
+const testTag = '£337_H4X0R';
+
+function getPseudoRoot(pseudo) {
+    const prev = pseudo.prev();
+    if (prev.type === 'pseudo') {
+        return getPseudoRoot(prev);
+    }
+    return prev;
+}
+
+function bumpInstance(node) {
+    let count = 2;
+    node.value = `${node.value.replace(/\*(\d+)$/, (match, currentCount) => {
+        // if this node is already bumped, set count to 1 more of that
+        count = parseInt(currentCount, 10) + 1;
+    })}*${count}`;
+}
+
+function unquote(string) {
+    return string.replace(/"|'/g, '');
+}
+
+// converts a selector into something that emmet can use
+function normaliseForEmmet(selector) {
+    console.log(selector, 'pre-normalised');
+    const normalised = selectorParser(parsedSelector => {
+        // some things are easier with the selector parser, other things not...
+        parsedSelector.walkUniversals(universal => {
             universal.value = 'div';
         });
 
-        selector.walkCombinators(combinator => {
-            if (combinator.value === ' ') {
-                combinator.value = '>';
-                combinator.parent.insertBefore(combinator, combinator.clone());
-                combinator.parent.insertBefore(combinator, combinator.next().clone());
-            }
-            if (combinator.value === '~') {
-                combinator.value = '+';
-                combinator.next().value += '*2';
+        if (parsedSelector.first.first.type === 'pseudo') {
+            parsedSelector.first.prepend(selectorParser.tag({ value: 'div' }));
+        }
+
+        parsedSelector.walkCombinators(combinator => {
+            switch (combinator.value) {
+                case ' ':
+                    // .red .blue .green => .red>.blue>.blue>.green>.green
+                    // this is in case there is also a `.red > .blue`
+                    // equivalent; means we can distinguish their effects
+                    combinator.value = '>div>'; // silly hack, because adding nodes confuses walk* methods
+                    break;
+                case '~':
+                    // .red ~ .blue => .red+.blue*2
+                    combinator.value = '+';
+                    combinator.next().value += '*2';
+                    break;
+                default: // do nothing
             }
         });
 
-        selector.walkAttributes(attribute => {
-            // eslint-disable-next-line default-case
+        // some pseudos are more easily handled with regexes below
+        parsedSelector.walkPseudos(pseudo => {
+            const pseudoRoot = getPseudoRoot(pseudo);
+            switch (pseudo.value) {
+                case ':first-child':
+                case ':last-child': {
+                    // p:first-child => div>p*2
+                    bumpInstance(pseudoRoot);
+                    pseudoRoot.value = `div>${pseudoRoot.value}`;
+                    pseudo.parent.removeChild(pseudo);
+                    break;
+                }
+                case ':first-of-type': {
+                    // p:first-of-type => p+p
+                    bumpInstance(pseudoRoot);
+                    pseudo.parent.removeChild(pseudo);
+                    break;
+                }
+                case ':empty': {
+                    // p:empty => p+p{£337_H4X0R}
+                    pseudoRoot.value = `${pseudoRoot.value}{${testString}}+${pseudoRoot.value}`;
+                    pseudo.parent.removeChild(pseudo);
+                    break;
+                }
+                default: // do nothing
+            }
+        });
+
+        parsedSelector.walkAttributes(attribute => {
             switch (attribute.operator) {
                 case '~=':
-                    attribute.value = `'${attribute.raws.unquoted} test'`;
+                    // [colour~=red] => [colour~='red £337_H4X0R']
+                    attribute.value = `'${attribute.raws.unquoted} ${testString}'`;
                     break;
                 case '|=':
-                    attribute.value = `'${attribute.raws.unquoted}-test'`;
+                    // [colour|=red] => [colour|='red-£337_H4X0R']
+                    attribute.value = `'${attribute.raws.unquoted}-${testString}'`;
                     break;
                 case '^=':
-                    attribute.value = `'${attribute.raws.unquoted}test test'`;
+                    // [colour^=red] => [colour^='red£337_H4X0R £337_H4X0R']
+                    attribute.value = `'${attribute.raws.unquoted}${testString} ${testString}'`;
                     break;
                 case '$=':
-                    attribute.value = `'test test${attribute.raws.unquoted}'`;
+                    // [colour$=red] => [colour$='£337_H4X0R £337_H4X0Rred']
+                    attribute.value = `'${testString} ${testString}${attribute.raws.unquoted}'`;
                     break;
                 case '*=':
-                    attribute.value = `'test${attribute.raws.unquoted}test'`;
+                    // [colour*=red] => [colour*='£337_H4X0Rred£337_H4X0R']
+                    attribute.value = `'${testString}${attribute.raws.unquoted}${testString}'`;
                     break;
+                default: // do nothing
             }
+            // all operators should be `=` for emmet
             attribute.operator = '=';
         });
-    }
+    }).process(selector, { lossless: false }).result
+        .replace(/:(first-child|last-child)/g, (match, pseudo, offset, string) =>
+            `[${pseudo}]${string.slice(offset + match.length)}+${string.slice(0, offset)}`
+        )
+        .replace(/:(checked|disabled)/g, (match, pseudo, offset, string) =>
+            `[${pseudo}]${string.slice(offset + match.length)}+${string.slice(0, offset)}`
+        )
+        .replace(/:(enabled)/g, (match, pseudo, offset, string) =>
+            `[disabled]${string.slice(offset + match.length)}+${string.slice(0, offset)}`
+        )
+;
+    console.log(normalised, 'normalised');
+    return normalised;
+}
 
+export default async (originalCSS) => {
     return postcss().process(originalCSS).then(processedCSS => {
         const selectorPromises = [];
 
         processedCSS.root.walkRules((rule) => {
             selectorPromises.push(...rule.selector.split(',').map(selector =>
-                selectorParser(normaliseForEmmet).process(selector, { lossless: false })
+                normaliseForEmmet(selector)
             ));
         });
 
         return Promise.all(selectorPromises)
-            .then(selectors => selectors.map(selector => selector.result))
-            // .then(selectors => {
-            //     console.log(selectors);
-            //     return selectors;
-            // })
+            // .then(selectors => selectors.map(selector => selector.result))
+            .then(selectors => {
+                // console.log(selectors);
+                return selectors;
+            })
             .then(selectors => uniq(selectors))
             .then(selectors => selectors.reduce((src, selector) =>
-                `${src} ${emmet.expandAbbreviation(selector).replace(/\$\{0\}/g, '')}`
+                `${src} ${emmet.expandAbbreviation(selector, emmetProfile).replace(/\$\{0\}/g, '')}`
             , ''))
             .then(html => html.trim());
     });
---
+++ css-to-dom.spec.js
@@ -0,0 +1,115 @@
+import victorica from 'victorica';
+
+import cssToDom from './css-to-dom';
+
+const testString = '£337_H4X0R';
+
+const test = async (css, html) => {
+    const htmlFromCSS = await cssToDom(css);
+    expect(victorica(htmlFromCSS)).toBe(victorica(html));
+};
+
+it('expands tags correctly', async () => {
+    await test('p {}', '<p></p>');
+    await test('* {}', '<div></div>');
+    await test('p > * {}', '<p><div></div></p>');
+});
+
+it('expands classes correctly', async () => {
+    await test('.red {}', '<div class="red"></div>');
+    await test('.red.blue {}', '<div class="red blue"></div>');
+});
+
+it('expands IDs correctly', async () => {
+    await test('#red {}', '<div id="red"></div>');
+});
+
+it('expands attributes correctly', async () => {
+    await test('[colour] {}', '<div colour=""></div>');
+    await test('p[colour] {}', '<p colour=""></p>');
+
+    await test('[colour=red] {}', '<div colour="red"></div>');
+    await test('[colour="red"] {}', '<div colour="red"></div>');
+
+    await test('p[colour=red] {}', '<p colour="red"></p>');
+    await test('p[colour="red"] {}', '<p colour="red"></p>');
+
+    await test('[colour~=red] {}', `<div colour="red ${testString}"></div>`);
+    await test('[colour~="red"] {}', `<div colour="red ${testString}"></div>`);
+
+    await test('[colour|=red] {}', `<div colour="red-${testString}"></div>`);
+    await test('[colour|="red"] {}', `<div colour="red-${testString}"></div>`);
+
+    await test('[colour^=red] {}', `<div colour="red${testString} ${testString}"></div>`);
+    await test('[colour^="red"] {}', `<div colour="red${testString} ${testString}"></div>`);
+
+    await test('[colour$=red] {}', `<div colour="${testString} ${testString}red"></div>`);
+    await test('[colour$="red"] {}', `<div colour="${testString} ${testString}red"></div>`);
+
+    await test('[colour*=red] {}', `<div colour="${testString}red${testString}"></div>`);
+    await test('[colour*="red"] {}', `<div colour="${testString}red${testString}"></div>`);
+});
+
+it('nests descendents correctly', async () => {
+    await test('.red > .blue > .green {}', `
+        <div class="red">
+            <div class="blue">
+                <div class="green"></div>
+            </div>
+        </div>
+    `);
+
+    // create multiple depths of child/decendent selectors.
+    // this is so we can distinguish between possible occurences
+    // of otherwise identical child and decendent selectors
+    await test('.red .blue .green {}', `
+        <div class="red">
+            <div>
+                <div class="blue">
+                    <div>
+                        <div class="green"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+});
+
+it('nests siblings correctly', async () => {
+    await test('.red + .blue + .green {}', '<div class="red"></div><div class="blue"></div><div class="green"></div>');
+
+    // since we don't know if there will be an equivalent adjacent sibling selector,
+    // we have to be able check that it would override the general sibling
+    await test('.red ~ .blue ~ .green {}', `
+        <div class="red"></div>
+        <div class="blue"></div>
+        <div class="blue"></div>
+        <div class="green"></div>
+        <div class="green"></div>
+    `);
+});
+
+it('expands pseudos correctly', async () => {
+    await test('p:empty {}', `<p>${testString}</p><p></p>`);
+    await test('p:first-child {}', '<div><p></p><p></p></div>');
+    await test('p:last-child {}', '<div><p></p><p></p></div>');
+    await test(':first-of-type {}', '<div></div><div></div>');
+    await test('input[type=radio]:checked {}', '<input type="radio" checked="checked"></input><input type="radio"></input>');
+    await test('input:disabled {}', '<input disabled="disabled"></input><input></input>');
+    await test('input:enabled {}', '<input disabled="disabled"></input><input></input>');
+});
+
+it('expands complex selectors correctly', async () => {
+    await test('.red > p ~ i {}', `
+        <div class="red">
+            <p></p>
+            <i></i>
+            <i></i>
+        </div>
+    `);
+});
+
+it('expands chained selectors correctly', async () => {
+    await test('p, i {}', '<p></p><i></i>');
+    await test('.red > q ~ q, i {}', '<div class="red"><q></q><q></q><q></q></div><i></i>');
+});
